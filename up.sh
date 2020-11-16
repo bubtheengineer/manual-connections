cec9e11f0bd70c7b812018ba37f6d8ca38de0c65
bub <57535312+bubtheengineer@users.noreply.github.com>
2020-11-15 22:42:28 -0500

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,4 +1,5 @@
 # Make sure transmission is stopped and update it's IP address to that of the PIA tunnel.
 service transmission onestop
-sed s/IP_ADDRESS/$1/ /usr/local/etc/transmission/home/settings_template.json > /usr/local/etc/transmission/home/settings.json
+ip=`ifconfig $1 | grep 'inet' | cut -d' ' -f2`
+sed s/IP_ADDRESS/$ip/ /usr/local/etc/transmission/home/settings_template.json > /usr/local/etc/transmission/home/settings.json
 service transmission onestart
